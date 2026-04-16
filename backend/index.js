20d6c11ea65506dc6a63581424dd2bcca035d8e4
Harshtomar21 <harsh.tomar@digimonk.in>
2026-04-16 21:39:23 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -41,8 +41,7 @@ app.use((req, res, next) => {
 app.use(cors({
   origin: "*",
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true
+  allowedHeaders: ["Content-Type", "Authorization"]
 }));
 
 app.use(express.json());
